1dfecfd71ec4d616bf717e2f1e242d481c39be7f
Owen Xie <15828145+nfusionz@users.noreply.github.com>
2021-05-03 14:40:04 -0400

    Add slurm spectrum

---
+++ src/slurmSpectrum.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+# sbatch -N $nodes --ntasks-per-node=$tasks --partition=dcs --gres=gpu:1,nvme -t 5 ./slurmSpectrum.sh cudagensim.exe
+
+if [ "x$SLURM_NPROCS" = "x" ]
+then
+    if [ "x$SLURM_NTASKS_PER_NODE" = "x" ]
+    then
+        SLURM_NTASKS_PER_NODE=1
+    fi
+    SLURM_NPROCS=`expr $SLURM_JOB_NUM_NODES \* $SLURM_NTASKS_PER_NODE`
+    else
+        if [ "x$SLURM_NTASKS_PER_NODE" = "x" ]
+        then
+            SLURM_NTASKS_PER_NODE=`expr $SLURM_NPROCS / $SLURM_JOB_NUM_NODES`
+        fi
+fi
+
+
+srun hostname -s | sort -u > /tmp/hosts.$SLURM_JOB_ID
+awk "{ print \$0 \"-ib slots=$SLURM_NTASKS_PER_NODE\"; }" /tmp/hosts.$SLURM_JOB_ID > /tmp/tmp.$SLURM_JOB_ID
+mv /tmp/tmp.$SLURM_JOB_ID /tmp/hosts.$SLURM_JOB_ID
+
+# module load gcc/7.4.0/1
+# module load spectrum-mpi
+# module load cuda
+module load xl_r spectrum-mpi cuda/10.2
+
+cpu_list="0"
+end_iter="$(expr $SLURM_NPROCS - 1)"
+for i in $(seq 1 $end_iter)
+do
+    index="$(expr 4 \* $i)"
+    cpu_list="$cpu_list,$index"
+done
+
+# first command line arg is locaiton of the program to run
+# second command line arg is the number of thousands of bytes to run this on
+# third command line arg can be anything - this tells us to use NVME
+if [ -n "$3" ]; then
+    taskset --cpu-list "$cpu_list" mpirun -hostfile /tmp/hosts.$SLURM_JOB_ID -np $SLURM_NPROCS $1 $2 $SLURM_JOB_ID
+else
+    taskset --cpu-list "$cpu_list" mpirun -hostfile /tmp/hosts.$SLURM_JOB_ID -np $SLURM_NPROCS $1 $2
+fi
+rm /tmp/hosts.$SLURM_JOB_ID
